40523b54456c50b7842b8e73fba5e3fef82026a7
reasat-rafio <rafioar96@gmail.com>
2024-07-07 18:53:13 +0600

    🔨 Footer adjustment

--- src/components/common/ContactInfoLinks/ContactInfo.tsx
+++ src/components/common/ContactInfoLinks/ContactInfo.tsx
@@ -1,3 +1,6 @@
+import Link from 'next/link';
+import PageTransitionLink from '../ui/PageTransitionLink';
+
 interface ContactInfoProps {}
 
 const ContactInfo: React.FC<ContactInfoProps> = ({}) => {
@@ -5,24 +8,27 @@ const ContactInfo: React.FC<ContactInfoProps> = ({}) => {
     <div className="mt-[80px] flex flex-col-reverse justify-between p-5 md:flex-row lg:mt-[200px]">
       <div className="flex items-end gap-2.5 max-md:justify-between max-md:pt-8">
         <span className="flex flex-col gap-[15px]">
-          <h3 className="m-0 cursor-default p-[2.5px] text-xs font-light text-[grey]">
+          {/* <h3 className="m-0 cursor-default p-[2.5px] text-xs font-light text-[grey]">
             Version
-          </h3>
+          </h3> */}
           <p className='m-0 cursor-pointer p-[2.5px] text-sm after:relative after:left-2/4 after:mt-[2px] after:block after:h-px after:w-[0%] after:-translate-x-2/4 after:bg-[white] after:transition-[width] after:duration-[0.2s] after:ease-linear after:content-[""] hover:after:w-full'>
-            2024 © Edition
+            ©2024 ALPINE
           </p>
         </span>
       </div>
       <div className="flex items-end gap-2.5 border-b-[rgb(134,134,134)]/50 max-md:border-b max-md:pb-8">
         <span className="flex flex-col gap-[15px]">
-          <h3 className="m-0 cursor-default p-[2.5px] text-xs font-light text-[grey]">
+          {/* <h3 className="m-0 cursor-default p-[2.5px] text-xs font-light text-[grey]">
             Socials
-          </h3>
-          <p className='m-0 cursor-pointer p-[2.5px] text-sm after:relative after:left-2/4 after:mt-[2px] after:block after:h-px after:w-[0%] after:-translate-x-2/4 after:bg-[white] after:transition-[width] after:duration-[0.2s] after:ease-linear after:content-[""] hover:after:w-full'>
-            Awwwards
-          </p>
+          </h3> */}
+          <PageTransitionLink
+            href="/privacy"
+            className='m-0 block cursor-pointer p-[2.5px] text-sm after:relative after:left-2/4 after:mt-[2px] after:block after:h-px after:w-[0%] after:-translate-x-2/4 after:bg-[white] after:transition-[width] after:duration-[0.2s] after:ease-linear after:content-[""] hover:after:w-full'
+          >
+            Privacy
+          </PageTransitionLink>
         </span>
-        <p className='m-0 cursor-pointer p-[2.5px] text-sm after:relative after:left-2/4 after:mt-[2px] after:block after:h-px after:w-[0%] after:-translate-x-2/4 after:bg-[white] after:transition-[width] after:duration-[0.2s] after:ease-linear after:content-[""] hover:after:w-full'>
+        {/* <p className='m-0 cursor-pointer p-[2.5px] text-sm after:relative after:left-2/4 after:mt-[2px] after:block after:h-px after:w-[0%] after:-translate-x-2/4 after:bg-[white] after:transition-[width] after:duration-[0.2s] after:ease-linear after:content-[""] hover:after:w-full'>
           Instagram
         </p>
         <p className='m-0 cursor-pointer p-[2.5px] text-sm after:relative after:left-2/4 after:mt-[2px] after:block after:h-px after:w-[0%] after:-translate-x-2/4 after:bg-[white] after:transition-[width] after:duration-[0.2s] after:ease-linear after:content-[""] hover:after:w-full'>
@@ -30,7 +36,7 @@ const ContactInfo: React.FC<ContactInfoProps> = ({}) => {
         </p>
         <p className='m-0 cursor-pointer p-[2.5px] text-sm after:relative after:left-2/4 after:mt-[2px] after:block after:h-px after:w-[0%] after:-translate-x-2/4 after:bg-[white] after:transition-[width] after:duration-[0.2s] after:ease-linear after:content-[""] hover:after:w-full'>
           Linkedin
-        </p>
+        </p> */}
       </div>
     </div>
   );

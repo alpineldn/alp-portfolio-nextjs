b05469250d2a811da76245137266ea62dcd1d938
reasat-rafio <rafioar96@gmail.com>
2024-07-07 17:17:52 +0600

    🔍️ Favicon and touch icon added

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -3,10 +3,15 @@ import './globals.css';
 import localFont from '@next/font/local';
 import cn from '@/utils/cn';
 import SmoothScroll from '@/components/common/SmoothScroll/SmoothScroll';
+import { Metadata } from 'next';
 
 interface RootLayoutProps {
   children: React.ReactNode;
 }
+
+// export const metadata: Metadata = {
+//   icons: {},
+// };
 
 const ppNeueMontreal = localFont({
   src: [
@@ -26,6 +31,10 @@ const ppNeueMontreal = localFont({
 const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
   return (
     <html lang="en">
+      {/* <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" type="image/x-icon" />
+      </head> */}
+
       <body className={cn(ppNeueMontreal.variable, 'font-ppneuemontreal')}>
         <SmoothScroll>
           <Header />

83fbbffc8ea0afbafc01b8c4d0511ea040784367
reasat-rafio <rafioar96@gmail.com>
2024-07-08 21:25:25 +0600

    🔨 Cta button added to the second section

--- src/components/landing-page/Description/index.tsx
+++ src/components/landing-page/Description/index.tsx
@@ -3,6 +3,8 @@
 import { useInView, motion } from 'framer-motion';
 import { useRef } from 'react';
 import { slideUp, opacity } from './animation';
+import PageTransitionLink from '@/components/common/ui/PageTransitionLink';
+import MarqueeText from '@/components/common/ui/MarqueeText';
 
 interface DescriptionProps {}
 
@@ -15,36 +17,47 @@ const Description: React.FC<DescriptionProps> = () => {
   return (
     <section
       ref={description}
-      className="text-light relative z-10 flex justify-center bg-dark pt-[100px] lg:pt-[200px]"
+      className="relative z-10 bg-dark pt-[100px] text-light lg:pt-[200px]"
     >
-      <div className="container relative flex flex-col gap-12 md:flex-row">
-        <p className="m-0 gap-3 text-3xl leading-[1.3] lg:text-4xl">
-          {phrase.split(' ').map((word, index) => {
-            return (
-              <span
-                key={index}
-                className="relative mr-[7px] inline-flex overflow-hidden"
-              >
-                <motion.span
-                  variants={slideUp}
-                  custom={index}
-                  animate={isInView ? 'open' : 'closed'}
+      <div className="container relative mx-auto">
+        <div className="flex flex-col gap-12 md:flex-row">
+          <p className="m-0 gap-3 text-3xl leading-[1.3] lg:text-4xl">
+            {phrase.split(' ').map((word, index) => {
+              return (
+                <span
                   key={index}
+                  className="relative mr-[7px] inline-flex overflow-hidden"
                 >
-                  {word}
-                </motion.span>
-              </span>
-            );
-          })}
-        </p>
-        <motion.p
-          className="m-0 max-w-64 text-sm md:max-w-full lg:text-base"
-          variants={opacity}
-          animate={isInView ? 'open' : 'closed'}
-        >
-          The combination of my passion for design, code & interaction positions
-          me in a unique place in the web design world.
-        </motion.p>
+                  <motion.span
+                    variants={slideUp}
+                    custom={index}
+                    animate={isInView ? 'open' : 'closed'}
+                    key={index}
+                  >
+                    {word}
+                  </motion.span>
+                </span>
+              );
+            })}
+          </p>
+          <motion.p
+            className="m-0 max-w-64 text-sm md:max-w-full lg:text-base"
+            variants={opacity}
+            animate={isInView ? 'open' : 'closed'}
+          >
+            The combination of my passion for design, code & interaction
+            positions me in a unique place in the web design world.
+          </motion.p>
+        </div>
+
+        <motion.div variants={opacity} animate={isInView ? 'open' : 'closed'}>
+          <PageTransitionLink
+            className="mt-12 block w-fit lg:mt-32"
+            href="/about"
+          >
+            <MarqueeText innerClassName="">About Us ↗</MarqueeText>
+          </PageTransitionLink>
+        </motion.div>
       </div>
     </section>
   );

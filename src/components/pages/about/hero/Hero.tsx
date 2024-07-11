dbb3198748454225885fb4d9c6d0ddf16b63d3eb
reasat-rafio <rafioar96@gmail.com>
2024-07-11 21:53:26 +0600

    🩹 Fixed the menu icon not showing in the about page issue

--- src/components/pages/about/hero/Hero.tsx
+++ src/components/pages/about/hero/Hero.tsx
@@ -1,8 +1,42 @@
+'use client';
+
+import { useStore } from '@/store/store';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useLayoutEffect, useRef } from 'react';
+
 interface HeroProps {}
 
 const Hero: React.FC<HeroProps> = ({}) => {
+  const containerRef = useRef<HTMLElement>(null);
+  const { setShowMenuButton } = useStore((store) => store);
+
+  useLayoutEffect(() => {
+    const context = gsap.context(() => {
+      if (!containerRef?.current) return;
+
+      gsap.registerPlugin(ScrollTrigger);
+      ScrollTrigger.create({
+        trigger: containerRef.current,
+        start: 'top top',
+        end: 'bottom top',
+        onLeave: () => {
+          setShowMenuButton(true);
+        },
+        onEnterBack: () => {
+          setShowMenuButton(false);
+        },
+      });
+    });
+
+    return () => context.revert();
+  }, [containerRef]);
+
   return (
-    <section className="min-h-screen pt-[9.88rem] md:pt-[11.75rem] xl:pt-[12.75rem]">
+    <section
+      ref={containerRef}
+      className="min-h-screen pt-[9.88rem] md:pt-[11.75rem] xl:pt-[12.75rem]"
+    >
       <div className="flex flex-col gap-[5.62rem] max-lg:px-5 sm:gap-[7rem] lg:gap-[9rem] lg:px-[8vw]">
         <h1 className="h1 lg:max-w-7xl">
           Lorem ipsum dolor sit amet consectetur.

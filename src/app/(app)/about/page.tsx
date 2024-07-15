3541f8234e1e973462f3896127b88ea89fdecaf2
reasat-rafio <rafioar96@gmail.com>
2024-07-15 23:27:01 +0600

    🔍️ Missing metadata added to the about page

--- src/app/(app)/about/page.tsx
+++ src/app/(app)/about/page.tsx
@@ -4,18 +4,25 @@ import Hero from '@/components/pages/about/hero/Hero';
 import Service from '@/components/pages/about/service/Service';
 import { SITE_URL } from '@/utils/constants';
 import generateMeta from '@/utils/generate-meta';
+import sanityClient from '@/utils/sanity/client';
+import { Metadata } from 'next';
+import { Meta } from '../page';
+import { META_QUERY } from '@/utils/sanity/queries';
 
-export const metadata = generateMeta({
-  title: 'About',
-  description: '',
-  og: {
-    type: 'website',
-    url: `${SITE_URL}/about`,
-    localImg: {
-      showTitle: true,
+export async function generateMetadata({}): Promise<Metadata> {
+  const metaData: Meta = await sanityClient.fetch(META_QUERY('/about'));
+
+  return generateMeta({
+    title: metaData?.title ?? 'Alpine',
+    description: metaData?.meta.description ?? "Alpine's work page.",
+    og: {
+      type: 'website',
+      url: `${SITE_URL}/about`,
+      localImg: '/images/flowers.jpg',
     },
-  },
-});
+    keywords: metaData?.meta?.keywords,
+  });
+}
 
 const AboutPage: React.FC<{}> = () => {
   return (

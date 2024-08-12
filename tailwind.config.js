ddbcb1bbece606f16dd9aa3f2113515e07348d66
reasat-rafio <rafioar96@gmail.com>
2024-08-12 23:22:05 +0600

    🔨 Work detail description layout fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -72,6 +72,15 @@ module.exports = {
       },
       gridTemplateColumns: {
         16: 'repeat(16, minmax(0, 1fr))',
+        15: 'repeat(15, minmax(0, 1fr))',
+        14: 'repeat(14, minmax(0, 1fr))',
+        13: 'repeat(13, minmax(0, 1fr))',
+      },
+      gridColumn: {
+        'span-16': 'span 16 / span 16',
+        'span-15': 'span 15 / span 15',
+        'span-14': 'span 14 / span 14',
+        'span-13': 'span 13 / span 13',
       },
 
       colors: {

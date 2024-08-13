254ae52a4dd8dc20dcd62cc07fc457f8aa22da2c
alpdigital <alparsonage@gmail.com>
2024-08-13 13:37:08 +0700

    change heading size on project list layout

--- src/components/common/project-layouts/ListLayout/Project.tsx
+++ src/components/common/project-layouts/ListLayout/Project.tsx
@@ -27,7 +27,7 @@ const ProjectList: React.FC<ProjectProps> = ({
       }}
       className="interactable group grid w-full cursor-pointer items-center gap-2 border-t border-solid border-t-white/50 py-xs text-white transition-all duration-300 last:border-b last:border-b-white/50 hover:opacity-50 max-lg:grid-cols-12 md:gap-5 lg:grid-flow-col 2xl:py-sm"
     >
-      <h3 className="heading-xl col-span-12 lg:col-span-12">{title}</h3>
+      <h3 className="heading-l col-span-12 lg:col-span-12">{title}</h3>
       <div className="heading-m col-span-12 block md:col-span-6">{client}</div>
       <div className="heading-m col-span-12 block md:col-span-6 md:text-right">
         {categories.map((category, index) => (

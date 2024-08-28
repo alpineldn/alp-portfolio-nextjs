a0bc037c14d69891eed6c877e65ec8f4b177241e
reasat-rafio <rafioar96@gmail.com>
2024-08-28 15:26:52 +0600

    💫 Animation added to the services section

--- src/components/pages/about/service/Description.tsx
+++ src/components/pages/about/service/Description.tsx
@@ -1,4 +1,7 @@
 import cn from '@/utils/cn';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useLayoutEffect, useRef } from 'react';
 
 interface DescriptionProps {
   index: number;
@@ -11,12 +14,41 @@ const Description: React.FC<DescriptionProps> = ({
   index,
   title,
 }) => {
+  const descriptionRef = useRef<HTMLParagraphElement>(null);
+
+  useLayoutEffect(() => {
+    const context = gsap.context(() => {
+      if (!descriptionRef?.current) return;
+
+      gsap.registerPlugin(ScrollTrigger);
+
+      gsap.fromTo(
+        descriptionRef.current,
+        { opacity: 0, y: 50 },
+        {
+          opacity: 1,
+          y: 0,
+          scrollTrigger: {
+            trigger: descriptionRef.current,
+            start: 'top 80%',
+            end: 'bottom 20%',
+            scrub: true,
+          },
+        },
+      );
+    });
+
+    return () => context.revert();
+  }, [descriptionRef]);
+
   return (
     <div className={cn('space-y-8 md:max-w-[65%] xl:max-w-[55%]')}>
       <h3 className="heading-xl">
         <span className="text-gray">{index + 1}.</span> {title}
       </h3>
-      <p className="heading-m">{description}</p>
+      <p ref={descriptionRef} className="heading-m">
+        {description}
+      </p>
     </div>
   );
 };

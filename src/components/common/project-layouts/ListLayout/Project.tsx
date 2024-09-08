81022d6089cebe52a5d1093e6bf6bc06a65c8c73
reasat-rafio <rafioar96@gmail.com>
2024-09-08 14:54:55 +0600

    ✨ Media added to the project list on mobile screen

--- src/components/common/project-layouts/ListLayout/Project.tsx
+++ src/components/common/project-layouts/ListLayout/Project.tsx
@@ -1,6 +1,10 @@
 import { Project as ProjectTypes } from '@/app/(app)/work/page';
-import React from 'react';
+import React, { useLayoutEffect, useRef, useState } from 'react';
 import PageTransitionLink from '../../ui/PageTransitionLink';
+import Video from '../../video/Video';
+import SanityImage from '../../sanity-image/SanityImage';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 interface ProjectProps extends ProjectTypes {
   index: number;
@@ -14,7 +18,35 @@ const ProjectList: React.FC<ProjectProps> = ({
   categories,
   client,
   slug,
+  mainImage,
+  tileMedia,
 }) => {
+  const img = tileMedia?.tileImage ?? mainImage;
+  const video = tileMedia?.tileVideo;
+  const mediaContainerRef = useRef<HTMLDivElement>(null);
+  const [sectionIntersection, setSectionIntersection] = useState(false);
+
+  useLayoutEffect(() => {
+    const context = gsap.context(() => {
+      if (!mediaContainerRef?.current) return;
+
+      gsap.registerPlugin(ScrollTrigger);
+
+      ScrollTrigger.create({
+        trigger: mediaContainerRef.current,
+        start: 'top 80%',
+        onEnter: () => {
+          setSectionIntersection(true);
+        },
+        onLeaveBack: () => {
+          setSectionIntersection(false);
+        },
+      });
+    });
+
+    return () => context.revert();
+  }, [mediaContainerRef]);
+
   return (
     <PageTransitionLink
       href={`work/${slug.current}`}
@@ -25,19 +57,35 @@ const ProjectList: React.FC<ProjectProps> = ({
       onMouseLeave={(e) => {
         manageModal(false, index, e.clientX, e.clientY);
       }}
-      className="interactable group grid w-full cursor-pointer items-center gap-2 border-t border-solid border-t-white/50 py-xs text-white transition-all duration-300 last:border-b last:border-b-white/50 hover:opacity-50 max-lg:grid-cols-12 md:gap-5 lg:grid-cols-12 2xl:py-sm"
+      className="interactable group grid w-full cursor-pointer grid-cols-2 items-center gap-2 border-t border-solid border-t-white/50 py-xs text-white transition-all duration-300 last:border-b last:border-b-white/50 hover:opacity-50 lg:grid-cols-1 2xl:py-sm"
     >
-      <h3 className="col-span-12 text-l lg:col-span-5">{title}</h3>
-      <div className="col-span-12 block text-m max-lg:hidden md:col-span-6 lg:col-span-4">
-        {client}
+      <div className="grid max-lg:grid-cols-12 md:gap-5 lg:grid-cols-12">
+        <h3 className="col-span-12 text-l lg:col-span-5">{title}</h3>
+        <div className="col-span-12 block text-m max-lg:hidden md:col-span-6 lg:col-span-4">
+          {client}
+        </div>
+        <div className="col-span-12 block text-m md:col-span-6 lg:col-span-3 xl:text-right">
+          {categories.map((category, index) => (
+            <span key={category._id}>
+              {category.title}
+              {index !== categories.length - 1 && ', '}
+            </span>
+          ))}
+        </div>
       </div>
-      <div className="col-span-12 block text-m md:col-span-6 lg:col-span-3 xl:text-right">
-        {categories.map((category, index) => (
-          <span key={category._id}>
-            {category.title}
-            {index !== categories.length - 1 && ', '}
-          </span>
-        ))}
+      <div ref={mediaContainerRef} className="block lg:hidden">
+        <div className="flex aspect-[16/12] h-full w-full items-center justify-center">
+          {!!video ? (
+            <Video active={sectionIntersection} {...video} />
+          ) : (
+            <SanityImage
+              sizes="50vw"
+              src={img}
+              alt={title}
+              className="h-auto w-auto object-cover transition-transform duration-500 ease-smooth-curve group-hover:scale-105"
+            />
+          )}
+        </div>
       </div>
     </PageTransitionLink>
   );

17ae9c1fc9656c1ca75d775e819cf0a88ae68206
Al Reasat Rafio <zyk0n_b@d.lan>
2024-09-15 18:06:19 +0600

    :dizzy: animation added to the services title

--- src/components/pages/about/service/Description.tsx
+++ src/components/pages/about/service/Description.tsx
@@ -14,38 +14,43 @@ const Description: React.FC<DescriptionProps> = ({
   index,
   title,
 }) => {
+  const sectionRef = useRef<HTMLDivElement>(null);
+  const headerRef = useRef<HTMLHeadingElement>(null);
   const descriptionRef = useRef<HTMLParagraphElement>(null);
 
   useLayoutEffect(() => {
     const context = gsap.context(() => {
-      if (!descriptionRef?.current) return;
+      if (!descriptionRef?.current || !sectionRef.current || !headerRef.current)
+        return;
 
       gsap.registerPlugin(ScrollTrigger);
 
-      gsap.fromTo(
-        descriptionRef.current,
-        { opacity: 0, y: 50 },
-        {
-          opacity: 1,
-          y: 0,
-          scrollTrigger: {
-            trigger: descriptionRef.current,
-            start: 'top 80%',
-            end: 'bottom 20%',
-          },
+      ScrollTrigger.create({
+        trigger: sectionRef.current,
+        start: 'top 80%',
+        end: 'bottom 20%',
+        onEnter: () => {
+          gsap.to(headerRef.current, { opacity: 1, y: 0, delay: 0.1 });
+          gsap.to(descriptionRef.current, { opacity: 1, y: 0, delay: 0.3 });
         },
-      );
+      });
     });
 
     return () => context.revert();
-  }, [descriptionRef]);
+  }, [descriptionRef, sectionRef, headerRef]);
 
   return (
-    <div className={cn('space-y-8 md:max-w-[65%] xl:max-w-[55%]')}>
-      <h3 className="text-xl">
+    <div
+      ref={sectionRef}
+      className={cn('space-y-8 md:max-w-[65%] xl:max-w-[55%]')}
+    >
+      <h3 ref={headerRef} className={cn('translate-y-12 opacity-0', 'text-xl')}>
         <span className="text-gray">{index + 1}.</span> {title}
       </h3>
-      <p ref={descriptionRef} className="text-m">
+      <p
+        ref={descriptionRef}
+        className={cn('translate-y-12 opacity-0', 'text-m')}
+      >
         {description}
       </p>
     </div>

0333895afaca9e5d14930002b26c71cd782856f8
reasat-rafio <rafioar96@gmail.com>
2024-09-16 01:49:56 +0600

    tweak

--- tailwind.config.js
+++ tailwind.config.js
@@ -118,7 +118,6 @@ module.exports = {
 
       colors: {
         primary: '#D12245',
-        'primary-accent': '#E14265',
         'primary-hover': '#B11A39',
         'primary-active': '#991C38',
         white: '#FFFFFF',

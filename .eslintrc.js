580a7890a12e96e8cf693151df33a9e3113de269
alpdigital <alparsonage@gmail.com>
2024-10-22 12:57:57 +0700

    Fix ESLint configuration and install required plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,24 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    extends: [
-      'plugin:@typescript-eslint/recommended',
-      'plugin:react/recommended',
-      'plugin:react-hooks/recommended',
-      'plugin:jsx-a11y/recommended',
-      'prettier',
-      'prettier/@typescript-eslint',
-      'prettier/react'
-    ],
-    plugins: ['@typescript-eslint'],
-    rules: {
-      // your custom rules
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended', // TypeScript rules
+    'plugin:react/recommended', // React rules
+    'plugin:react-hooks/recommended', // React hooks rules
+    'plugin:jsx-a11y/recommended', // Accessibility rules
+    'prettier', // Prettier config (for disabling incompatible ESLint rules)
+  ],
+  plugins: [
+    '@typescript-eslint', // TypeScript plugin
+    'react', // React plugin
+    'react-hooks', // React hooks plugin
+    'jsx-a11y', // Accessibility plugin
+  ],
+  rules: {
+    // Add your custom ESLint rules here
+  },
+  settings: {
+    react: {
+      version: 'detect', // Automatically detect the React version
     },
-  };
-  
+  },
+};

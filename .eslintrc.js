8641b32fe1fd8a3cc5e35163571098c3461906db
alpdigital <alparsonage@gmail.com>
2024-10-23 12:29:27 +0700

    eslint no-empty-object-type off

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
   ],
   plugins: ['@typescript-eslint', 'react', 'react-hooks', 'jsx-a11y', 'import'],
   rules: {
+    '@typescript-eslint/no-unused-vars': 'off', // Disable the no-unused-vars rule globally
     '@typescript-eslint/no-empty-object-type': 'off', // Disable the no-empty-object-type rule
   },
   settings: {
